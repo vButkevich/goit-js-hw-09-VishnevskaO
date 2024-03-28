40a2e58bcb38cc076cdcbdeb9abc6f12a6ec65ac
vButkevich <33177042+vButkevich@users.noreply.github.com>
2024-03-28 11:35:16 +0000

    Deploying to gh-pages from @ vButkevich/goit-js-hw-09-VishnevskaO@b87d02a85866b1e8e2c4f6165a93aa635dc7e537 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/styles-186f96ea.js";import{S as o}from"./assets/vendor-870f0eb5.js";new o(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});
+//# sourceMappingURL=commonHelpers.js.map
